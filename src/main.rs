8f294da0b08321a1a5b6cc3020d4924853c4fc38
git314 <git314@tutanota.com>
2021-04-14 01:27:39 +0000

    pull values from arrow array

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
 use std::fs::File;
-use std::fmt::Debug;
-use std::any::Any;
-
 use arrow::csv;
-use arrow::error::Result as ArrowResult;
+use arrow::array::{as_primitive_array, as_string_array, as_null_array};
+use arrow::datatypes::*;
 
-fn main()-> ArrowResult<()> {
-   reader_builder_example()?;
-
-   Ok(())
+fn main() {
+   reader_builder_example();
 }
 
-fn reader_builder_example() -> ArrowResult<()>{
+fn reader_builder_example(){
     /* d.csv
     "carat","cut","depth"
     0.23,"Ideal",55
@@ -21,23 +17,45 @@ fn reader_builder_example() -> ArrowResult<()>{
     let csv_builder = csv::ReaderBuilder::new()
         .has_header(true)
         .infer_schema(Some(100));
-    let mut csv_reader = csv_builder.build(file)?;
-    let batch = csv_reader.next().unwrap().unwrap();
-    let schema = batch.schema();
-    let num_columns = batch.num_columns();
+    let mut csv_reader = csv_builder.build(file).unwrap();
+    let record_batch = csv_reader.next().unwrap().unwrap();
+    let schema = record_batch.schema();
+    let num_rows = record_batch.num_rows();
+    let num_columns = record_batch.num_columns();
+    println!("# A tv: {} x {}", num_rows, num_columns);
     for i in 0..num_columns{
-        print!("{:} ",schema.field(i).name());
+        print!("{:} ", schema.field(i).name());
     }
     println!();
     for i in 0..num_columns{
-        print!("{:} ",schema.field(i).data_type());
+        print!("<{}> ", schema.field(i).data_type());
     }
     println!();
-    for i in 0..num_columns{
-        let col = batch.column(i);
-        println!("{:?} ",col);
-        println!();
+    // must be a better way than to read twice
+    let file = File::open("d.csv").unwrap();
+    let csv_builder = csv::ReaderBuilder::new()
+        .has_header(true)
+        .infer_schema(Some(100));
+    let csv_reader = csv_builder.build(file).unwrap();
+    //
+    for batch in csv_reader {
+        for column in batch.unwrap().columns(){
+                match column.data_type() {
+                DataType::Int8    => print!("{:?} ",as_primitive_array::<Int8Type>(column).values()),
+                DataType::Int16   => print!("{:?} ",as_primitive_array::<Int16Type>(column).values()),
+                DataType::Int32   => print!("{:?} ",as_primitive_array::<Int32Type>(column).values()),
+                DataType::Int64   => print!("{:?} ",as_primitive_array::<Int64Type>(column).values()),
+                DataType::UInt8   => print!("{:?} ",as_primitive_array::<UInt8Type>(column).values()),
+                DataType::UInt16  => print!("{:?} ",as_primitive_array::<UInt16Type>(column).values()),
+                DataType::UInt32  => print!("{:?} ",as_primitive_array::<UInt32Type>(column).values()),
+                DataType::UInt64  => print!("{:?} ",as_primitive_array::<UInt64Type>(column).values()),
+                DataType::Float32 => print!("{:?} ",as_primitive_array::<Float32Type>(column).values()),
+                DataType::Float64 => print!("{:?} ",as_primitive_array::<Float64Type>(column).values()),
+                DataType::Null    => print!("{:?} ",as_null_array(column)),
+                DataType::Utf8    => print!("{:} ",as_string_array(column).value(0)),
+                _ => println!("NA")
+            }
+        }
     }
-
-    Ok(())
+    println!();
 }
